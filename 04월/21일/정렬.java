3a2a91cd3960ea0b2f2eda8fe2bebe79dc2a875e
limtowoong <104752202+limtowoong@users.noreply.github.com>
2022-05-03 16:24:15 +0900

    정렬

--- 04월/21일/정렬.java
+++ 04월/21일/정렬.java
@@ -4,21 +4,21 @@ public class SelectSort_0421 {
 
 	public static void main(String[] args) {
 		
-		int A[] = {18, 5, 21, 2, 7};		//난수 a에 18, 5, 21, 2, 7를 넣음
-		int min = 0;			//min 선언 (최소값)
-		int temp;			//temp 선언 (임시로 저장할 값)
+		int A[] = {18, 5, 21, 2, 7};				//난수 a에 18, 5, 21, 2, 7를 넣음
+		int min = 0;						//min 선언 (최소값)
+		int temp;						//temp 선언 (임시로 저장할 값)
 		
 		System.out.println("최초 배열의 상태");
 		System.out.println("-------------------------------------------");
 		for(int i=0; i<A.length; i++) {
-			System.out.print(A[i]+" ");
+			System.out.print(A[i]+" ");					//최초 배열의 상태를 출력
 		}
 		System.out.println("");
 		System.out.println("-------------------------------------------");
 		
 		for(int num=0; num<A.length; num++) {
 			System.out.println("A["+num+"] 자리에 넣을 최소값을 찾습니다.");
-			min = num;
+			min = num;								//min에 num값을 넣음
 			System.out.println("현재 min: "+min+", A[min]: "+A[min]);
 			System.out.println("-------------------------------------------");
 			
