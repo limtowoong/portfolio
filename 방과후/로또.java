e2caf1990dc769b31afa217e6e2e3e727ed7f8ae
limtowoong <104752202+limtowoong@users.noreply.github.com>
2022-06-09 15:04:31 +0900

    Update 로또.java

--- 방과후/로또.java
+++ 방과후/로또.java
@@ -1,4 +1,4 @@
-package examAverage;
+package lotto;
 import java.util.Random;
 
 public class LottoP {
@@ -9,25 +9,26 @@ public class LottoP {
 		Random r = new Random();
 
 		// 1~45 숫자 중에 랜덤으로 6개를 뽑아내라!
-		lotto[0] = r.nextInt(44)+1;
-		for(int i=1; i<lotto.length; i++) {
+		for(int i=0; i<lotto.length; i++) {
 			lotto[i] = r.nextInt(44)+1;
-			for(int j=i; j<=0; j--) { // i번째 이전의 수를 훑는 반복문
+			for(int j=0; j<i; j++) { // i번째 이전의 수를 훑는 반복문
 				// 중복이 있냐?				
 				if(lotto[i]==lotto[j]) {
 					i--;
 				}
 			}				
 		}
-		
-		for(int j=0; j<lotto.length; j++) {
-			System.out.printf("%d  ", lotto[j]);
+
+		for(int i=0; i<lotto.length; i++) {
+			System.out.printf("%d  ", lotto[i]);
 		}
+		System.out.println();
+		
+		
 		
 		// 값을 정렬해보자 - 버블 정렬
 		for(int a=1; a<lotto.length; a++) {
 			for(int i=0; i<lotto.length-1; i++) {
-				System.out.println(i+":");
 				if(lotto[i]>lotto[i+1]) {
 					int temp = lotto[i];
 					lotto[i] = lotto[i+1];
@@ -35,7 +36,6 @@ public class LottoP {
 				}
 			}			
 		}
-		
 		
 		// 값을 출력
 		for(int i=0; i<lotto.length; i++) {
